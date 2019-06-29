fcb27457ac6c5f5775202b18e5e8bef2166065c8
yjjung@mac <yjjung@tecace.com>
2019-06-30 01:37:47 +0900

    add .

---
+++ soocore/src/pages/approve.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+   approve
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'approve',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
