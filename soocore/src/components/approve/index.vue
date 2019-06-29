c2c3dd4e56a86a0de39bf9c88c51613f765e0b90
yjjung@mac <yjjung@tecace.com>
2019-06-30 06:19:20 +0900

    remove trailing space

--- soocore/src/components/approve/index.vue
+++ soocore/src/components/approve/index.vue
@@ -6,7 +6,6 @@
 
 <script>
 export default {
-  name: '',
   data () {
     return {
      
@@ -15,6 +14,6 @@ export default {
 }
 </script>
 
-<style scoped>g
+<style scoped>
 
 </style>
--- soocore/src/components/main/index.vue
+++ soocore/src/components/main/index.vue
@@ -5,7 +5,6 @@
 
 <script>
 export default {
-  name: '',
   data () {
     return {
       
