473e54ae5611e518b0e59a955d30f556226225a7
yjjung@mac <yjjung@tecace.com>
2019-07-12 21:21:25 +0900

    add nano scroll to add.vue

--- soocore/src/pages/add.vue
+++ soocore/src/pages/add.vue
@@ -1,39 +1,44 @@
 
 <template>
 <div class="flex auto column">
-  <div class="sub-title">추가</div>
-  <div class="flex none content column" >
-    <div class="flex none justify-content-start align-items-center add-title"> 나의 역할</div>
-    <div class="flex" style="width:70%; margin:0 auto;">
-      <div class="flex auto justify-content-center">
-        <div class="flex none circle justify-content-center align-items-center" :style="{
-          background:role == 1 ? '#D8695E' : '#D9D9D9'
-        }" @click="role=1">갑</div>
+  <div class="nano">
+    <div class="nano-content">
+      <div class="sub-title">추가</div>
+      <div class="flex none content column" >
+        <div class="flex none justify-content-start align-items-center add-title"> 나의 역할</div>
+        <div class="flex" style="width:70%; margin:0 auto;">
+          <div class="flex auto justify-content-center">
+            <div class="flex none circle justify-content-center align-items-center" :style="{
+              background:role == 1 ? '#D8695E' : '#D9D9D9'
+            }" @click="role=1">갑</div>
+          </div>
+          <div class="flex auto justify-content-center">
+            <div class="flex none justify-content-center align-items-center circle" :style="{
+              background:role == 2 ? '#F2D677' : '#D9D9D9'
+            }" @click="role=2">을</div>
+          </div>
+        </div>
       </div>
-      <div class="flex auto justify-content-center">
-        <div class="flex none justify-content-center align-items-center circle" :style="{
-          background:role == 2 ? '#F2D677' : '#D9D9D9'
-        }" @click="role=2">을</div>
+      <div class="flex column none content" style="position: relative;">
+        <div class="flex none justify-content-start align-items-center add-title"> 목표 점수</div>
+        <div ref="scoreSelectorBg" 
+        @touchstart="onMouseDown" @touchmove="onMouseMove" @touchend="onMouseUp"
+        @mousedown="onMouseDown" @mousemove="onMouseMove" @mouseup="onMouseUp" @mouseleave="onMouseUp" 
+        class="flex justify-content-start score-selector-bg">
+          <div ref="scoreSelector" id="scoreSelector" class="score-selector">
+          </div>
+          <div class="flex align-items-center justify-content-center score-selector-text" style="font-size:3vw; left:3%;">1,000</div>
+          <div class="flex align-items-center justify-content-center score-selector-text goal-score">{{goalScore}} 점</div>
+          <div class="flex align-items-center justify-content-center score-selector-text" style="font-size:3vw; left:96%;">100,000</div>
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="flex column none content" style="position: relative;">
-    <div class="flex none justify-content-start align-items-center add-title"> 목표 점수</div>
-    <div ref="scoreSelectorBg" 
-    @touchstart="onMouseDown" @touchmove="onMouseMove" @touchend="onMouseUp"
-    @mousedown="onMouseDown" @mousemove="onMouseMove" @mouseup="onMouseUp" @mouseleave="onMouseUp" 
-    class="flex justify-content-start score-selector-bg">
-      <div ref="scoreSelector" id="scoreSelector" class="score-selector">
+      <div class="flex content column" >
+        <div class="flex none justify-content-start align-items-center add-title"> 달성 보상</div>
+        <div class="flex auto" style="width:90%; margin-left:5%;">
+          <textarea style="width:100%;" placeholder="달성 보상을 입력해 주세요." type="text" rows="4" cols="30"/>
+        </div>
       </div>
-      <div class="flex align-items-center justify-content-center score-selector-text" style="font-size:3vw; left:3%;">1,000</div>
-      <div class="flex align-items-center justify-content-center score-selector-text goal-score">{{goalScore}} 점</div>
-      <div class="flex align-items-center justify-content-center score-selector-text" style="font-size:3vw; left:96%;">100,000</div>
-    </div>
-  </div>
-
-   <div class="flex content column" >
-    <div class="flex none justify-content-start align-items-center add-title"> 달성 보상</div>
-    <div class="flex auto" style="width:90%; margin-left:5%;"><textarea style="width:100%;" placeholder="달성 보상을 입력해 주세요." type="text" rows="4" cols="30"/></div>
+   </div>
   </div>
 </div>
 
