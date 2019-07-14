4962c732e8a4041a6b684cc1fc7f95025e883f82
yjjung@mac <yjjung@tecace.com>
2019-07-14 22:07:59 +0900

    remove mouseup interval

--- soocore/src/components/add-score-popup/index.vue
+++ soocore/src/components/add-score-popup/index.vue
@@ -4,15 +4,14 @@
       <div class="flex none align-items-center add-title" style="margin-bottom:2vw;"> 점수 </div>
       <div class="flex">
         <div class="flex none clickable">
-          <div @mouseup="onMouseUp(1, $event)" @touchup="onMouseUp(1, $event)" 
-          @mousedown="onMouseDown(1, $event)" @touchdown="onMouseDown(1, $event)" class="flex none justify-content-center align-items-center icon-button" style="width:12vw; height:12vw; border-radius:4vw 0 0 4vw;">
+          <div
+            @mousedown="onMouseDown(1, $event)" @touchdown="onMouseDown(1, $event)" class="flex none justify-content-center align-items-center icon-button" style="width:12vw; height:12vw; border-radius:4vw 0 0 4vw;">
             <div class="flex none icon-angle-down" style="font-size:8vw;"></div> 
           </div>
           <input class="flex align-items-center" type="number" style="color:#2D3642; font-size:8vw; font-weight:bold;border:0.5vw solid #D8695E; border-width: 0.5vw 0 0.5vw 0; width:40vw; padding:0 1vw; outline:none;" v-model="point" @input="point = point<0 ? 0 : point>100000 ? 100000 : point">
           
           <div
-          @mouseup="onMouseUp(2, $event)" @touchup="onMouseUp(2, $event)" 
-          @mousedown="onMouseDown(2, $event)" @touchdown="onMouseDown(2, $event)" class="flex none justify-content-center align-items-center icon-button" style="width:12vw; height:12vw; border-radius:0 4vw 4vw 0;">
+            @mousedown="onMouseDown(2, $event)" @touchdown="onMouseDown(2, $event)" class="flex none justify-content-center align-items-center icon-button" style="width:12vw; height:12vw; border-radius:0 4vw 4vw 0;">
             <div class="flex none icon-angle-up" style="font-size:8vw;"></div>
           </div>
         </div>
@@ -56,20 +55,8 @@ export default {
       if(type==1){ this.point-=100}
       else{ this.point+=100 }
       this.point = this.point < 0 ? 0 : this.point>100000 ? 100000 : this.point
-      if(this.mouseDownInterval){
-        clearInterval(this.mouseDownInterval)
-      }
-      this.mouseDownInterval = setInterval(function(that){
-        if(type==1){ that.point-=100}
-        else{ that.point+=100 }
-        that.point = that.point < 0 ? 0 : that.point>100000 ? 100000 : that.point
-      },200, this)
     },
-    onMouseUp(type){
-      if(this.mouseDownInterval){
-        clearInterval(this.mouseDownInterval)
-      }
-    }
+   
   },
   mounted(){
    
